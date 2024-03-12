860f19975a052e2fbdd2f1e7a63495cc4858af96
alaz <akaleli1@binghamton.edu>
2024-03-12 13:34:08 -0700

    Model updated

--- app.py
+++ app.py
@@ -13,32 +13,13 @@ nltk.download('wordnet')
 
 app = Flask(__name__)
 
-df = pd.read_csv('bullying_tweets.csv')
+df = pd.read_csv('cleaned_tweets.csv')
 
 X_train, X_test, y_train, y_test = train_test_split(df['tweet_text'], df['bully_status'], random_state=42)
     
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        # Get the tweet from the user
-        tweet = request.form['tweet']
-        # Make a prediction
-        file = 'bullying_model2.sav'
-        loaded_model = pickle.load(open(file, 'rb'))
-        # print(stopwords.words('english')[:20])
-        tfidf_vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'), max_features=1000)
-        # print(df['cleaned_tweet_text_without_hashtags'])
-        # tweetText = tfidf_vectorizer.fit_transform(df['tweet_text'])
-        input = [tweet]
-        input_data = tfidf_vectorizer.transform(input)
-
-        predictions = loaded_model.predict(input_data)
-        prediction = "Bullying" if predictions[0] == 1 else "Not Bullying"
-        return render_template('index.html', prediction=prediction)
 
 
 @app.route('/prediction', methods=['POST'])
@@ -47,13 +28,11 @@ def prediction():
         # Get the tweet from the user
         tweet = request.form['tweet']
         # Make a prediction
-        filename = 'bully_model_NB.sav'
-        clf = pickle.load(open(filename, 'rb'))
-        count_vector = CountVectorizer(stop_words = 'english', lowercase = True, ngram_range=(1,1))
-        training_data = count_vector.fit_transform(X_train)
-        input = count_vector.transform([tweet])
+        clf = pickle.load(open('NB_Model.sav', 'rb'))
+        vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
+        
+        input = vectorizer.transform([tweet])
         predictions = clf.predict(input)
         prediction = "Bullying" if predictions[0] == 1 else "Not Bullying"
         return render_template('index.html', prediction=prediction)
-        
         
